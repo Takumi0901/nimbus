a69b6b992ab4c3a455fe694fcf7c69e9929693c9
takumi kimura <juventus0901@gmail.com>
2017-03-24 12:36:03 +0900

    added modal

--- assets/js/common.js
+++ assets/js/common.js
@@ -31,4 +31,144 @@ jQuery(document).ready(function($){
     $('body,html').animate({scrollTop:position}, speed, 'swing');
     return false;
   });
+
+  /**
+   * modal
+   */
+  $('.js-modal-btn').each(function(){
+    new Modal(this);
+  });
 });
+
+//　モーダル
+////////////////////////////////////////////////////////////
+function Modal(el){
+  this.initialize(el);
+  this.handleEvents();
+}
+
+Modal.prototype.initialize = function(el){
+  this.$el = $(el);
+  this.$modal = $('.js-modal');
+  this.$overlay = $('.js-modal-overlay');
+  this.$closeBtn = $('.js-modal-close-btn');
+}
+
+Modal.prototype.handleEvents = function(){
+  var self = this;
+
+  var event = 'ontouchstart' in window ? 'touchstart' : 'click';
+
+  this.$el.on(event, function(event){
+    event.preventDefault();
+    var $target = self.$el.attr('href');
+    self.modalFadeIn($target);
+
+    return false;
+  });
+
+  this.$overlay.on(event, function(event){
+    self.modalFadeOut();
+
+    return false;
+  });
+
+  this.$closeBtn.on(event, function(event){
+    self.modalFadeOut();
+
+    return false;
+  });
+}
+
+Modal.prototype.modalFadeIn = function(target){
+  $(target).addClass('is-fadein');
+}
+
+Modal.prototype.modalFadeOut = function(){
+  var self = this;
+
+  this.$modal.addClass('is-fadeout');
+
+  setTimeout(function(){
+    self.modalFadeReset();
+  },600);
+}
+
+Modal.prototype.modalFadeReset = function(){
+  this.$modal.removeClass('is-fadein');
+  this.$modal.removeClass('is-fadeout');
+}
+
+function FixedContent(){
+  this.initialize();
+  this.handleScrollEvents();
+}
+
+FixedContent.prototype.initialize = function(){
+  this.$sideH = $('.js-fixed-menu').innerHeight();
+  this.$mainH = $('.main_content').innerHeight();
+  this.$sideOffsetTop = $('.js-fixed-menu').offset().top
+  this.$bottomContentH = $('.side_bottom').innerHeight();
+
+
+  var self = this;
+  this.sideWithOverFlowScroll();
+
+  // $(window).resize(function(){
+  //   self.sideWithOverFlowScroll();
+  // });
+}
+
+FixedContent.prototype.sideWithOverFlowScroll = function(){
+  this.$wH = $(window).height();
+
+  if(this.$wH < this.$sideH){
+    $('.side_content_inner').css({'height': this.$wH});
+    $('.side_menu_wrap').css('height', this.$wH - this.$bottomContentH);
+  }else{
+    $('.side_content_inner').css({'height': 'auto'});
+    $('.side_menu_wrap').css('height', 'auto');
+  }
+}
+
+FixedContent.prototype.handleScrollEvents = function(){
+  var self = this
+  $(window).on('scroll', function(){
+    var scrollValue = $(this).scrollTop();
+    if(self.$mainH > self.$sideH){
+      self.customScroll(scrollValue);
+    }
+  });
+}
+
+FixedContent.prototype.customScroll = function(scrollValue){
+  var self = this;
+  $('.js-fixed-menu').each(function(){
+    var $this = $(this);
+    var $targetEle = $('.copyright').offset().top;
+    var $targetPos = $targetEle - self.$wH;
+
+    if(self.$bottomContentH == null){
+      $targetPos = $targetEle - self.$wH;
+    }else{
+      $targetPos = $targetEle - self.$wH - 140;
+    }
+
+    if(self.$sideOffsetTop <= scrollValue){
+      if($targetPos < scrollValue){
+        if(!$this.hasClass('is-absolute')){
+          $this.addClass('is-absolute');
+          $this.removeClass('is-fixed');
+        }
+      }else{
+        if(!$this.hasClass('is-fixed')){
+          $this.addClass('is-fixed');
+          $this.removeClass('is-absolute');
+        }
+      }
+    }else{
+      $this.removeClass('is-fixed');
+      $this.removeClass('is-absolute');
+    }
+  });
+}
