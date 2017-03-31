c5d836d34ccb4a4921053b29ed5fee846b1760a7
takumi kimura <juventus0901@gmail.com>
2017-03-31 11:27:23 +0900

    gulp修正

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp'),
+    uglify = require("gulp-uglify"),
     postcss = require('gulp-postcss'),
     sourcemaps = require('gulp-sourcemaps'),
     rename = require("gulp-rename"),
@@ -14,23 +15,44 @@ gulp.task('css', function() {
     cssNext()
   ];
 
-  return gulp.src('./src/**.pcss')
+  return gulp.src('./src/css/**.pcss')
     .pipe(plumber())
     .pipe(postcss(preProcessors))
     .pipe(sourcemaps.init())
     .pipe(postcss([cssNano]))
     .pipe(rename('app.min.css'))
     .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('./docs/css'))
     .pipe(gulp.dest('./assets/css'));
 });
+
+gulp.task('js:base', function() {
+  return gulp.src('./src/js/**.js')
+    .pipe(plumber())
+    .pipe(gulp.dest('./assets/js'));
+});
+
+gulp.task('js:min', ['js:base'], function() {
+  return gulp.src('./src/js/common.js')
+    .pipe(plumber())
+    .pipe(uglify())
+    .pipe(rename('common.min.js'))
+    .pipe(gulp.dest('./docs/js'))
+    .pipe(gulp.dest('./assets/js'));
+});
+
+gulp.task('js', ['js:base', 'js:min']);
 
 /**
  * watch
  */
 gulp.task('watch', function(){
-    gulp.watch('./src/**/**/**.pcss', function(event) {
-        gulp.run('css');
-    });
+  gulp.watch('./src/css/**/**/**.pcss', function(event) {
+      gulp.run('css');
+  });
+  gulp.watch('./src/js/**.js', function(event) {
+    gulp.run('js');
+  });
 });
 
 gulp.task('default', function(){
